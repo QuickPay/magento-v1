e3e15e49493f29295bab141a4bb742e8f10101ca
Marek <marek.svorobovic@gmail.com>
2021-05-30 10:14:27 +0300

    change default admin translations to english

--- app/code/community/Quickpay/Payment/Model/System/Config/Source/PaymentMethods.php
+++ app/code/community/Quickpay/Payment/Model/System/Config/Source/PaymentMethods.php
@@ -4,9 +4,9 @@ class QuickPay_Payment_Model_System_Config_Source_PaymentMethods
     public function toOptionArray()
     {
         return array(
-            array('value' => '', 'label' => Mage::helper('quickpaypayment')->__('Alle betalingsmetoder')),
-            array('value' => 'creditcard', 'label' => Mage::helper('quickpaypayment')->__('Alle kreditkort')),
-            array('value' => 'specified', 'label' => Mage::helper('quickpaypayment')->__('Som angivet')),
+            array('value' => '', 'label' => Mage::helper('quickpaypayment')->__('All payment methods')),
+            array('value' => 'creditcard', 'label' => Mage::helper('quickpaypayment')->__('All credit cards')),
+            array('value' => 'specified', 'label' => Mage::helper('quickpaypayment')->__('As defined')),
         );
     }
 }
